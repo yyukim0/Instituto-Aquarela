d6b14f470df4e79bac306caed5536fd496f44542
kaue <kyibiapino@gmail.com>
2026-07-31 01:39:02 -0300

    modificações leves no footer e drawer adicionado

--- home/js/index.js
+++ home/js/index.js
@@ -1,3 +1,5 @@
+//carrossel de imagens no index.html
+
 const track = document.querySelector('.moviment');
 const images = document.querySelectorAll('.moviment img');
 
@@ -16,3 +18,29 @@ function nextSlide() {
 }
 
 setInterval(nextSlide, delay);
+
+// coisinha que abre quando você clica no dropdown/drawer
+
+const dropdownBtn = document.querySelector('.image-dropdown');
+const sidebarMenu = document.getElementById('sidebarMenu');
+const menuOverlay = document.getElementById('menuOverlay');
+
+function openMenu() {
+    sidebarMenu.classList.add('open');
+    menuOverlay.classList.add('open');
+    document.body.style.overflow = 'hidden'; 
+}
+
+function closeMenu() {
+    sidebarMenu.classList.remove('open');
+    menuOverlay.classList.remove('open');
+    document.body.style.overflow = ''; 
+}
+
+if (dropdownBtn) {
+    dropdownBtn.addEventListener('click', openMenu);
+}
+
+if (menuOverlay) {
+    menuOverlay.addEventListener('click', closeMenu);
+}
